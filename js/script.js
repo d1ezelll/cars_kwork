f6d5554fc4903e8fb87312a04eda3f8df0bd9d24
d1ezelll <diezelll@mail.ru>
2024-12-13 13:25:56 +0400

    adaptation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
 document.getElementById('formSend').addEventListener('click', function() {
     document.getElementById('orderForm').reset();
+  }
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerBtn = document.getElementById('header-button');
+  const header = document.querySelector('.header');
+  const headerContacts = document.querySelector('.header-contacts');
+
+
+  burgerBtn.addEventListener('click', () => {
+      if (header.classList.toggle('open')) {
+          headerContacts.style.display = 'flex';
+          burgerBtn.textContent = 'Close menu'
+      } else {
+          headerContacts.style.display = 'none';
+          burgerBtn.textContent = 'Contact Us'
+      };
+  })
+
+  window.addEventListener('click', (e) => {
+      if (!header.contains(e.target) && !burgerBtn.contains(e.target)) {
+          header.classList.remove('open');
+      }
   });
+})
   
